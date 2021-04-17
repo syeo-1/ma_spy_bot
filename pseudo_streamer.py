a2b1e28d1e11594bfe13e7b6f5f0203940f72225
Sean Yeo <seansqyeo@gmail.com>
2021-04-16 22:18:11 -0700

    create plot_best_params function

--- pseudo_streamer.py
+++ pseudo_streamer.py
@@ -46,6 +46,30 @@ def param_guesser():
     print(f"best maxmin range: {best_maxmin_range}")
 
 
+def plot_best_params():
+    # plt.figure(figsize=(8,6))
+    fig, axs = plt.subplots(3)
+    fig.set_figheight(8)
+    fig.set_figwidth(8)
+    fig.suptitle('stock info!')
+    # fig(num=1, figsize=(8,6), dpi=80)
+    # axs[0].plot(bids)
+    axs[0].plot(sg_bids)
+    # x and y arrays must have the same size. ie, the must have the same number of elements!!!!
+    axs[0].scatter(buys_x,buys_y,c="green",zorder=2)
+    axs[0].scatter(sells_x,sells_y,c="red",zorder=2)
+    # test =  list(range(1000))
+
+    # axs[1].plot(bids_deriv1)
+    axs[1].plot(sg_bids_deriv1)
+    # axs[2].plot(bids_deriv2)
+    axs[2].plot(sg_bids_deriv2)
+    # for trade in trades:
+    #     plt.axvline(x=trade)
+    plt.ylabel('some numbers')
+    plt.show()
+
+
 def test_params(win_length, poly_order, maxmin_range):
     sg_bids = ss.savgol_filter(bids, 99, 3)
     bids_deriv1 = np.ediff1d(bids)
@@ -73,27 +97,7 @@ def test_params(win_length, poly_order, maxmin_range):
             sells_y.append(bids[i])
             sell = False
             buy = True
-    # plt.figure(figsize=(8,6))
-    fig, axs = plt.subplots(3)
-    fig.set_figheight(8)
-    fig.set_figwidth(8)
-    fig.suptitle('stock info!')
-    # fig(num=1, figsize=(8,6), dpi=80)
-    # axs[0].plot(bids)
-    axs[0].plot(sg_bids)
-    # x and y arrays must have the same size. ie, the must have the same number of elements!!!!
-    axs[0].scatter(buys_x,buys_y,c="green",zorder=2)
-    axs[0].scatter(sells_x,sells_y,c="red",zorder=2)
-    # test =  list(range(1000))
 
-    # axs[1].plot(bids_deriv1)
-    axs[1].plot(sg_bids_deriv1)
-    # axs[2].plot(bids_deriv2)
-    axs[2].plot(sg_bids_deriv2)
-    # for trade in trades:
-    #     plt.axvline(x=trade)
-    plt.ylabel('some numbers')
-    plt.show()
 
 if __name__ == "__main__":
     stream_data()
