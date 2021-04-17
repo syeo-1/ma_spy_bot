67524931fe9b17f8e01703f3f36d868f22496d31
Sean Yeo <seansqyeo@gmail.com>
2021-04-16 17:59:33 -0700

    create first derivative graph

--- pseudo_streamer.py
+++ pseudo_streamer.py
@@ -2,7 +2,7 @@
 import config
 import json
 import datetime
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as ss
 
@@ -19,11 +19,19 @@ def stream_data():
         print(f"bid: {data['data']['p']} ask: {data['data']['P']}")
         # current_date = datetime.datetime.fromtimestamp(float(data["data"]["t"])/1e9)
         i+=1
-        # if i == 5000:
-        #     break
+        if i == 100:
+            break
     sg_bids = ss.savgol_filter(bids, 99, 3)
-    plt.plot(bids)
-    plt.plot(sg_bids)
+    bids_deriv1 = np.ediff1d(bids)
+    sg_bids_deriv1 = ss.savgol_filter(bids, 99, 3, deriv=1)
+    # plt.plot(bids)
+    # plt.plot(sg_bids)
+    fig, axs = plt.subplots(2)
+    fig.suptitle('stock info!')
+    axs[0].plot(bids)
+    axs[0].plot(sg_bids)
+    axs[1].plot(bids_deriv1)
+    axs[1].plot(sg_bids_deriv1)
     # plt.plot(asks)
     plt.ylabel('some numbers')
     # ill be looking for the minimum ask prices and the maximum bid prices!!!!
