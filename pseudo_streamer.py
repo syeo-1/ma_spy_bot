64b7d4d178f1ea402f182716492a00dd2400c958
Sean Yeo <seansqyeo@gmail.com>
2021-04-16 17:37:38 -0700

    add basic sagol plot

--- pseudo_streamer.py
+++ pseudo_streamer.py
@@ -4,11 +4,11 @@ import json
 import datetime
 import numpy
 import matplotlib.pyplot as plt
+import scipy.signal as ss
 
 def stream_data():
     # i = 0
     bids = []
-    asks = []
     i = 0
     for data in open(config.RECORDED_DATA, "r"):
         data = json.loads(data)
@@ -19,9 +19,11 @@ def stream_data():
         print(f"bid: {data['data']['p']} ask: {data['data']['P']}")
         # current_date = datetime.datetime.fromtimestamp(float(data["data"]["t"])/1e9)
         i+=1
-        if i == 500:
-            break
+        # if i == 5000:
+        #     break
+    sg_bids = ss.savgol_filter(bids, 99, 3)
     plt.plot(bids)
+    plt.plot(sg_bids)
     # plt.plot(asks)
     plt.ylabel('some numbers')
     # ill be looking for the minimum ask prices and the maximum bid prices!!!!
