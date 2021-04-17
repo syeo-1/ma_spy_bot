293a1a882a344a702fc62f6f0ecc4501d356a37c
Sean Yeo <seansqyeo@gmail.com>
2021-04-16 21:05:40 -0700

    can superimpose buy scatterplot

--- pseudo_streamer.py
+++ pseudo_streamer.py
@@ -16,7 +16,7 @@ def stream_data():
         bids.append(data['data']['p'])
         # asks.append(data['data']['P'])
 
-        print(f"bid: {data['data']['p']} ask: {data['data']['P']}")
+        # print(f"bid: {data['data']['p']} ask: {data['data']['P']}")
         # current_date = datetime.datetime.fromtimestamp(float(data["data"]["t"])/1e9)
         i+=1
         # if i == 100:
@@ -28,17 +28,40 @@ def stream_data():
     sg_bids_deriv2 = ss.savgol_filter(bids, 99, 3, deriv=2)
     # plt.plot(bids)
     # plt.plot(sg_bids)
+    buy = True
+    sell = False
+    buys_x = []
+    buys_y = []
+    sells_x = []
+    sells_y = []
+    for i in range(len(bids)):
+        if -0.00001 < sg_bids_deriv1[i] < 0.00001 and sg_bids_deriv2[i] > 0 and buy:
+            print(f'BUY: {i}')
+            buys_x.append(i)
+            buys_y.append(bids[i])
+            buy = False
+            sell = True
+        elif -0.00001 < sg_bids_deriv1[i] < 0.00001 and sg_bids_deriv2[i] < 0 and sell:
+            print(f'SELL {i}')
+            sells_x.append(i)
+            sells_y.append(bids[i])
+            sell = False
+            buy = True
     fig, axs = plt.subplots(3)
     fig.suptitle('stock info!')
-    axs[0].plot(bids)
+    # axs[0].plot(bids)
     axs[0].plot(sg_bids)
-    axs[1].plot(bids_deriv1)
+    # x and y arrays must have the same size. ie, the must have the same number of elements!!!!
+    axs[0].scatter(buys_x,buys_y,c="green",zorder=2)
+    # test =  list(range(1000))
+
+    # axs[1].plot(bids_deriv1)
     axs[1].plot(sg_bids_deriv1)
-    axs[2].plot(bids_deriv2)
+    # axs[2].plot(bids_deriv2)
     axs[2].plot(sg_bids_deriv2)
-    # plt.plot(asks)
+    # for trade in trades:
+    #     plt.axvline(x=trade)
     plt.ylabel('some numbers')
-    # ill be looking for the minimum ask prices and the maximum bid prices!!!!
     plt.show()
 
 if __name__ == "__main__":
