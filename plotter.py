cb5f6683d192b9340ff24d0fb99d6e917505106c
Sean Yeo <seansqyeo@gmail.com>
2021-05-01 21:36:20 -0700

    add function for plotting recorded param vs profit data

--- plotter.py
+++ plotter.py
@@ -1,5 +1,7 @@
 '''use this file for creating plots with matplotlib'''
 import matplotlib.pyplot as plt
+import config
+import json
 
 def plot_dict_list(dict_ls, sortkey):
     ''' takes a list of dictionaries and plots linegraphs for the values '''
@@ -26,15 +28,61 @@ def plot_dict_list(dict_ls, sortkey):
     # show the plots
     plt.show()
 
+def plot_recorded_shared_data():
+    ''' read and plot the recorded shared data from shared_data file '''
+
+    # read the data
+    shared_data = []
+    with open(config.SHARED_DATA_FILE, 'r') as shared_data_file_contents:
+        for line in shared_data_file_contents:
+            shared_data.append(json.loads(line))
+    
+    plot_arrs = []
+    for key in shared_data[0]:
+        plot_arrs.append([data[key] for data in shared_data])
+    
+    for arr in plot_arrs:
+        plt.plot(arr)
+    
+    plt.show()
+
+
+
+
+
+
+
+
+
+def is_int(str_num):
+    try:
+        int_num = int(str_num)
+        float_num = float(str_num)
+    except (TypeError, ValueError):
+        return False
+    else:
+        return int_num == float_num
+
+def get_optimal_param_data(params):
+    pass
+
+
+
+def plot_optimal_params():
+    # get the optimal params stored in param_file.txt
+    param_file = open('param_file.txt', 'r')
+    
+    best_params = {}
+    for line in param_file:
+        data = line.strip().split(',')
+        if is_int(data[1]):
+            best_params[data[0]] = int(data[1])
+        else:
+            best_params[data[0]] = float(data[1])
+    
+    # print(best_params)
+
+    # create a plot using the best parameters
+
 if __name__ == '__main__':
-    a = [{
-        'a':1,
-        'b':2,
-        'c':3
-    },
-    {
-        'a':3,
-        'b':6,
-        'c':8
-    }]
-    plot_dict_list(a, 'a')
+    plot_recorded_shared_data()
